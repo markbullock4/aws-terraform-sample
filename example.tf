08a34bf13a4acb2f8b8eed80343aadf34f20f766
markbullock4 <48762574+markbullock4@users.noreply.github.com>
2020-02-11 17:09:30 +0000

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  aws_access_key_id     = "AKIA4EFBLASW67HI6CED"
-  aws_secret_access_key = "47U60PzBydQeVSe4ZeaPl50mMP/3k5ZnONe7NXyw"  
   region                = "eu-central-1"
 }
 
