260d4a84409442fafea5296968bb86933b9fbbae
markbullock4 <48762574+markbullock4@users.noreply.github.com>
2020-02-11 16:52:35 +0000

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,9 +1,10 @@
 provider "aws" {
   profile    = "default"
-  region     = "us-east-1"
+  region     = "eu-central-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-010fae13a16763bb4"
+  ami           = "ami-0c71ad703d12633ef"
   instance_type = "t2.micro"
+  security_groups = "sg-0121c2c731b6f2a3f" 
 }
