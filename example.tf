b4433f39848e63ba4f4a1ecf5f3c4d9bc3481aec
markbullock4 <48762574+markbullock4@users.noreply.github.com>
2020-02-11 16:28:47 +0000

    Update example.tf

--- example.tf
+++ example.tf
@@ -6,4 +6,6 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-010fae13a16763bb4"
   instance_type = "t2.micro"
+  vpc_id        = "vpc-08714bbcda913dc75"
+  subnet_id     = "subnet-061a451b3635cf2ad"
 }
